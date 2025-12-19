eea5b3af0106e389e73a57c15bede39e097cd1ad
codeshark-lucifer <codeshark.dev@gmail.com>
2025-12-20 06:43:36 +0700

    Setup Project

---
+++ build/CMakeFiles/application.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application.dir/src/main.cpp.obj"
+  "CMakeFiles/application.dir/src/main.cpp.obj.d"
+  "application.exe"
+  "application.exe.manifest"
+  "application.pdb"
+  "libapplication.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
